f203c229859f45ae39c08bd0112b759b9e639171
Ricardo Hernandez <ricahernandez0001@gmail.com>
2024-11-05 15:45:06 -0500

    Express JS with Secrets Page

---
+++ Express & NodeJS/3.5 Secrets Project/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function checkPassword(req, res, next) {
+    const password = req.body["password"];
+    console.log(password);
+    if (password === 'ILoveProgramming') {
+        res.sendFile(__dirname + "/public/secret.html");
+    } else {
+        res.redirect('/');
+    }
+    // Remove `next()` here to prevent further handling of the request
+}
+
+// Attach `checkPassword` only to the `/check` POST route
+app.post("/check", checkPassword);
+
+app.get("/", (req, res) => {
+    console.log("Hello World");
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
